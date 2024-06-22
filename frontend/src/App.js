803f4365a7ee490fb4457c5b6109504ce4304447
zarifRahman <zarif.shahriar.rahman@gmail.com>
2024-06-23 00:02:25 +0600

    package add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,21 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1> Welcome </h1>
-          <HomeScreen/>
+          <Outlet />
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
 export default App
